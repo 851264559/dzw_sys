f62b6e2824d08cdbf0422b36bd0876c8fa112979
ty <2323376639@qq.com>
2020-12-01 14:20:56 +0800

    ty后台

--- dzw_sys_services/src/main/java/com/accp/biz/ty/TYEmployeeBiz.java
+++ dzw_sys_services/src/main/java/com/accp/biz/ty/TYEmployeeBiz.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.accp.dao.ty.TYEmployeeDao;
+import com.accp.pojo.ty.Employee;
 import com.accp.pojo.ty.Ment;
 import com.accp.pojo.ty.Postb;
 import com.accp.pojo.ty.VO;
@@ -26,5 +27,30 @@ public class TYEmployeeBiz {
 	public PageInfo<VO> queryVoAll(@Param("bid")Integer bid,Integer pageNum, Integer pageSize){
 		PageHelper.startPage(pageNum, pageSize);
 		return new PageInfo<VO>(dao.queryAllVO(bid));
+	}
+	
+	//部门下拉框绑定
+	public List<Ment> queryMent(){
+		return dao.queryAllMent();
+	}
+	
+	//职位部门下拉框绑定
+	public List<Postb> queryPostb(){
+		return dao.queryAllPostb();
+	}
+	
+	//新增员工
+	public void addEmployee(Employee employee) {
+		dao.saveEdocentry(employee);
+	}
+	
+	//修改页面查询
+	public Employee getEmployee(Integer yid) {
+		return dao.loadEmployee(yid);
+	}
+	
+	//员工删除
+	public void removeyid(Integer yid) {
+		dao.deleteyid(yid);
 	}
 }
--- dzw_sys_web/src/main/java/com/accp/action/ty/TYEmployeeAction.java
+++ dzw_sys_web/src/main/java/com/accp/action/ty/TYEmployeeAction.java
@@ -1,15 +1,21 @@
 package com.accp.action.ty;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.accp.biz.ty.TYEmployeeBiz;
 import com.accp.biz.ty.TYMentBiz;
+import com.accp.pojo.ty.Employee;
 import com.accp.pojo.ty.Ment;
+import com.accp.pojo.ty.Postb;
 import com.accp.pojo.ty.VO;
 import com.github.pagehelper.PageInfo;
 
@@ -30,6 +36,43 @@ public class TYEmployeeAction {
 	@GetMapping("z/{bid}/{p}/{s}")
 	public PageInfo<VO> getVoAll(@PathVariable Integer bid,@PathVariable Integer p, @PathVariable Integer s) {
 		return biz.queryVoAll(bid, p, s);
+	}
+	
+	//部门下拉框绑定
+	@GetMapping("ww")
+	public List<Ment> getEdoVoInfo() {
+		return biz.queryMent();
+	}
+	
+	//职位下拉框绑定
+	@GetMapping("zw")
+	public List<Postb> getPostb() {
+		return biz.queryPostb();
+	}
+	
+	//新增
+	@PostMapping("employee")
+	public Map<String, String> addEmployeeInfo(@RequestBody Employee employee) {
+		Map<String, String> message = new HashMap<String, String>();
+		biz.addEmployee(employee);
+		message.put("code", "200");
+		message.put("msg", "ok");
+		return message;
+	}
+	
+	//修改页面详情
+	@GetMapping("cc/{yid}")
+	public Employee getEdocentry(@PathVariable Integer yid) {
+		return biz.getEmployee(yid);
+	}
+	
+	@DeleteMapping("hhh/{yid}")
+	public Map<String, String> delyidInfo(@PathVariable Integer yid) {
+		Map<String, String> message = new HashMap<String, String>();
+		biz.removeyid(yid);
+		message.put("code", "200");
+		message.put("msg", "ok");
+		return message;
 	}
 	
 }
