210a35770ed3b2342678359a45c1c6e2e28d8979
littleBearQvQ <2539878540@qq.com>
2020-12-10 10:14:34 +0800

    s

--- dzw_sys_web/src/main/java/com/accp/action/cwl/testOnCompletionAction.java
+++ dzw_sys_web/src/main/java/com/accp/action/cwl/testOnCompletionAction.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +34,10 @@ public class testOnCompletionAction {
 		@PutMapping("/modify")
 		public Integer modifyByPrimaryKeySelective(@RequestBody TestOnCompletion record) {
 			return tOCBiz.modifyByPrimaryKeySelective(record);
+		}
+		
+		@PostMapping("/add")
+		public int addSelective(@RequestBody TestOnCompletion record) {
+			return tOCBiz.addSelective(record);
 		}
 }
