b432694e45ae191f93cbb8463e7da5f0028eb405
c_linmo <c0102tao@163.com>
2020-12-11 10:39:46 +0800

    11111

--- dzw_sys_dao/src/main/java/com/accp/dao/cwl/closeanaccountMapper.java
+++ dzw_sys_dao/src/main/java/com/accp/dao/cwl/closeanaccountMapper.java
@@ -3,6 +3,7 @@ package com.accp.dao.cwl;
 import com.accp.pojo.cwl.closeanaccount;
 import com.accp.vo.ct.CloseanaccountVO;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.*;
 
@@ -24,4 +25,18 @@ public interface closeanaccountMapper {
     int updateByPrimaryKeySelective(closeanaccount record);
 
     int updateByPrimaryKey(closeanaccount record);
+
+    /**
+     * 每日现金收入
+     * @return
+     */
+    @Select("SELECT SUM(`clearingMoney`) FROM `closeanaccount` WHERE `timeOfCompletion` BETWEEN CURRENT_DATE AND CURRENT_DATE AND `clearing`=#{0} AND zhuangtai=#{1}")
+    int selectCount1(String s1,String s2);
+
+    /**
+     * 每日结单数量
+     * @return
+     */
+    @Select("SELECT COUNT(*) FROM `closeanaccount` WHERE `timeOfCompletion` BETWEEN CURRENT_DATE AND CURRENT_DATE AND zhuangtai=#{0}")
+    int selectCount2(String s1);
 }
